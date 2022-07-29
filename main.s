6202312fccccba2435f7347f90cb8bfa5ef85b63
Connor Horman <chorman64@gmail.com>
2022-07-29 12:44:21 -0400

    Fix syntax for specializations

--- main.s
+++ main.s
@@ -14,14 +14,14 @@ term_y:         .byte 0
 init_term:
     mov r2, 0x7f000000
     in double
-    test0, 0x00000001   ; I/O Subsystem enabled (likely redundant?)
+    test r0, 0x00000001   ; I/O Subsystem enabled (likely redundant?)
     jz spinlock
-    test0, 0x00000040   ; VGA flag
+    test r0, 0x00000040   ; VGA flag
     jz spinlock
 
     mov r2, 0x7f00000f  ; get VGA buffer address
     in double
-    add0 0x18000        ; offset to text mode area
+    add r0, 0x18000        ; offset to text mode area
     mov [vga_text_buf], r0
 
     ret
@@ -31,18 +31,18 @@ putc:
     mov r3, 80
     mul
     mov r1, [term_x]
-    add0 r1
-    lsh0 1
+    add r0, r1
+    lsh r0, 1
     mov [r0], r2
-    add0 1
+    add r0, 1
     mov [r0], 0x0f
 
-    add1 1
-    cmp1 80
+    add r1, 1
+    cmp r1, 80
     jlt putc$L1
     mov [term_x], 0
     mov r0, [term_y]
-    add0 1
+    add r0, 1
     mov [term_y], r0
 
 putc$L1:
