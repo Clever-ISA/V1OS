558319142a8a675d959972750e3fea1d82d0f88a
Ray Redondo <rdredondo@lcdev.xyz>
2022-07-29 16:35:15 +0000

    term: add putc

--- main.s
+++ main.s
@@ -1,9 +1,15 @@
 _start:
     call init_term
+    mov r2, 0x41        ; 'A'
+    call putc
 
     ;; TODO: Initialize filesystem, initialize keyboard, spin up a shell
 
     jmp spinlock
+
+vga_text_buf:   .long 0
+term_x:         .byte 0
+term_y:         .byte 0
 
 init_term:
     mov r2, 0x7f000000
@@ -13,8 +19,33 @@ init_term:
     test0, 0x00000040   ; VGA flag
     jz spinlock
 
-    ;; TODO: Load address of VGA buffer, initialize terminal state
+    mov r2, 0x7f00000f  ; get VGA buffer address
+    in double
+    add0 0x18000        ; offset to text mode area
+    mov [vga_text_buf], r0
 
+    ret
+
+putc:
+    mov r0, [term_y]
+    mov r3, 80
+    mul
+    mov r1, [term_x]
+    add0 r1
+    lsh0 1
+    mov [r0], r2
+    add0 1
+    mov [r0], 0x0f
+
+    add1 1
+    cmp1 80
+    jlt putc$L1
+    mov [term_x], 0
+    mov r0, [term_y]
+    add0 1
+    mov [term_y], r0
+
+putc$L1:
     ret
 
 spinlock:
